455efd615cf979dbe3899d216e2181afbc85bcfc
Chip Wolf <hello@chipwolf.uk>
2018-06-14 15:55:47 +0100

    :recycle: remove node dep. from tmux spotify track info script

---
+++ scripts/get_track.applescript
@@ -0,0 +1,16 @@
+on escape_quotes(string_to_escape)
+  set AppleScript's text item delimiters to the "\""
+  set the item_list to every text item of string_to_escape
+  set AppleScript's text item delimiters to the "\\\""
+  set string_to_escape to the item_list as string
+  set AppleScript's text item delimiters to ""
+  return string_to_escape
+end escape_quotes
+
+tell application "Spotify"
+  set ctrack to "{"
+  set ctrack to ctrack & "\"artist\":\"" & my escape_quotes(current track's artist) & "\""
+  set ctrack to ctrack & ",\"name\":\"" & my escape_quotes(current track's name) & "\""
+  set ctrack to ctrack & ",\"state\":\"" & player state & "\""
+  set ctrack to ctrack & "}"
+end tell
---
+++ scripts/spotinfo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+TRACK=$(osascript "$DIR/get_track.applescript")
+
+STATE=$(echo "$TRACK" | jq -r .state)
+TITLE="$(echo "$TRACK" | jq -r .name | sed 's/"/\\"/g')"
+ARTIST="$(echo "$TRACK" | jq -r .artist | sed 's/"/\\"/g')"
+
+if [[ "$STATE" == "playing" ]]; then
+  STATE="true"
+else
+  STATE="false"
+fi
+
+SEPERATOR="-"
+if [[ "$PLAYING" == "true"  ]]; then
+  PLAYSYMBOL="▶"
+else
+  PLAYSYMBOL="❚❚"
+fi
+if [[ "$TRACK" == "null" ]]; then
+  OUTPUT="◼"
+else
+  OUTPUT="$PLAYSYMBOL $ARTIST $SEPERATOR $TITLE"
+fi
+echo -n "$OUTPUT"
+
