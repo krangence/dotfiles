e06c0ba6be8e8a7dfb0829166dff7623f39bdc56
Chip Wolf <hello@chipwolf.uk>
2020-03-10 14:34:55 +0000

    :sparkles: casks and yabai upgrade + bubu combo

--- zsh/zsh-os-conf/osx-post-omz.zsh
+++ zsh/zsh-os-conf/osx-post-omz.zsh
@@ -5,6 +5,7 @@ PATH="$PATH:/usr/local/sbin"
 export GPG_TTY=$(tty)
 
 ### ALIASES
+alias bubus='if brew outdated | grep -q "yabai"; then brew services stop yabai; fi && bubu && brew cu --quiet --yes --no-brew-update && if ! pgrep -x "yabai" > /dev/null; then brew services start yabai && sudo yabai --uninstall-sa && sudo yabai --install-sa && killall {Dock,Finder}; fi'
 
 ### FUNCTIONS
 
